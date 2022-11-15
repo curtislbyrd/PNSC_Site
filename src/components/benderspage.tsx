5d08552acd04a86c80b71dd37d14f6275623da9d
Plexicast <58782596+curtislbyrd@users.noreply.github.com>
2022-11-15 15:17:21 -0500

    Update benderspage.tsx

--- src/components/benderspage.tsx
+++ src/components/benderspage.tsx
@@ -14,7 +14,7 @@ export const BendersPage: NextPage = () => {
       <main className={container.container}>
         <div className={container.topcontent}>
           <div className={container.whiteText}> Benders</div>
-           <div className={container.smallWhiteText}> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+           <div className={container.smallWhiteText}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
            </div>
           <div>
             <Button variant='outline'>
